73d0d369ed0e16a10dcc2044d3f1484064c2456e
ibnesaad-69 <ibnesaad28@gmail.com>
2025-11-03 23:26:13 +0600

    Add files via upload

--- scripts/cmds/train.js
+++ scripts/cmds/train.js
@@ -3,25 +3,26 @@ const fs = require("fs-extra");
 
 module.exports = {
   config: {
-    name: "trigger",
+    name: "train",
     version: "1.1",
-    author: "NTKhang",
+    author: "milan-says",
     countDown: 5,
     role: 0,
-    shortDescription: "Trigger image",
-    longDescription: "Trigger image",
+    shortDescription: "train image",
+    longDescription: "train image",
     category: "fun",
     guide: {
-      vi: "{pn} [@tag | để trống]",
-      en: "{pn} [@tag | empty]"
+      vi: "{pn} [@tag | blank]",
+      en: "{pn} [@tag]"
     }
   },
 
   onStart: async function ({ event, message, usersData }) {
-    const uid = Object.keys(event.mentions)[0] || event.senderID;
+ const uid = Object.keys(event.mentions)[0]
+ if(!uid) return message.reply("please mention someone")
     const avatarURL = await usersData.getAvatarUrl(uid);
-    const img = await new DIG.Triggered().getImage(avatarURL);
-    const pathSave = `${__dirname}/tmp/${uid}_Trigger.gif`;
+    const img = await new DIG.Thomas().getImage(avatarURL);
+ const pathSave = `${__dirname}/tmp/${uid}_Thomas.png`;
     fs.writeFileSync(pathSave, Buffer.from(img));
     message.reply({
       attachment: fs.createReadStream(pathSave)
--- scripts/cmds/trash2.js
+++ scripts/cmds/trash2.js
@@ -3,25 +3,25 @@ const fs = require("fs-extra");
 
 module.exports = {
   config: {
-    name: "trigger",
+    name: "trash2",
     version: "1.1",
-    author: "NTKhang",
+    author: "KSHITIZ",
     countDown: 5,
     role: 0,
-    shortDescription: "Trigger image",
-    longDescription: "Trigger image",
+    shortDescription: "",
+    longDescription: "",
     category: "fun",
     guide: {
       vi: "{pn} [@tag | để trống]",
-      en: "{pn} [@tag | empty]"
+      en: ""
     }
   },
 
   onStart: async function ({ event, message, usersData }) {
     const uid = Object.keys(event.mentions)[0] || event.senderID;
     const avatarURL = await usersData.getAvatarUrl(uid);
-    const img = await new DIG.Triggered().getImage(avatarURL);
-    const pathSave = `${__dirname}/tmp/${uid}_Trigger.gif`;
+    const img = await new DIG.Delete().getImage(avatarURL);
+    const pathSave = `${__dirname}/tmp/${uid}_delete.png`;
     fs.writeFileSync(pathSave, Buffer.from(img));
     message.reply({
       attachment: fs.createReadStream(pathSave)
